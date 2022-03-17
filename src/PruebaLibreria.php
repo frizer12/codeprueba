169971ef26e37190584e4098c2e4007b8c525dfe
Jose Federico <sistemas.sadeco@gmail.com>
2022-03-16 22:59:38 -0600

    first commit

---
+++ src/PruebaLibreria.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Federico\Pems;
+
+// require 'vendor/autoload.php';
+
+class PruebaLibreria
+{
+    public static function Primera()
+    {
+        return 'josé Federico Guerrero Rodriguez';
+    }
+}
---
+++ tests/TestUnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+// use Federico\Pems\PruebaLibreria;
+
+class TestUnitTest extends TestCast
+{
+    /** @test */
+    function test_unit_test()
+    {
+        // $prueba = PruebaLibreria::Primera();
+        // echo $prueba;
+        $this->assertTrue(true);
+    }
+}
